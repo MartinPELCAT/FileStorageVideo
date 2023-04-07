c713537e86ec06ca159dc361714ed942c02bb9e3
MartinPELCAT <martinpelcat@hotmail.com>
2023-04-07 11:52:18 +0200

    get pixels infos about image

--- decoder/src/utils/decoder.rs
+++ decoder/src/utils/decoder.rs
@@ -29,11 +29,49 @@ pub fn number_of_pixels_on_last_line(image: &DynamicImage) -> u8 {
 pub fn get_file_name(image: &DynamicImage) -> String {
     let number_of_chars_pixel = image.get_pixel(3, 0);
 
-    String::from("")
+    let number_of_chars = get_number_of_chars_of_the_title(number_of_chars_pixel);
+
+    // This is the number of chars in hex
+    let number_of_pixels_to_take = number_of_chars * 2;
+
+    // This is the number of valid chars on the last pixel
+    let last_pixel_chars = number_of_pixels_to_take % 6;
+
+    let number_of_pixels_of_the_title = get_number_of_pixels_of_the_title(number_of_chars);
+
+    let mut title_hexa_value = String::from("");
+
+    for pixel_index in 4..(4 + number_of_pixels_of_the_title) {
+        let title_pixel = image.get_pixel(pixel_index as u32, 0);
+
+        let hex = rbg_to_hex(title_pixel);
+        title_hexa_value.push_str(&hex);
+    }
+
+    let number_of_chars_to_remove = 6 - last_pixel_chars;
+
+    title_hexa_value.truncate(title_hexa_value.len() - number_of_chars_to_remove as usize);
+    let title = hex::decode(&title_hexa_value).unwrap();
+
+    String::from_utf8(title).unwrap()
 }
 
 fn get_number_of_chars_of_the_title(pixel: Rgba<u8>) -> u8 {
-    0
+    pixel.0[0]
+}
+fn get_number_of_pixels_of_the_title(number_of_chars: u8) -> u8 {
+    let number_of_pixels_to_take = number_of_chars * 2;
+
+    // This is the number of valid chars on the last pixel
+    let last_pixel_chars = number_of_pixels_to_take % 6;
+
+    let mut number_of_pixels_of_the_title = number_of_pixels_to_take / 6;
+
+    if last_pixel_chars != 0 {
+        number_of_pixels_of_the_title += 1;
+    }
+
+    number_of_pixels_of_the_title
 }
 
 fn rbg_to_hex(rgba: Rgba<u8>) -> String {
